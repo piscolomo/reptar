4f4225d93791be1aa6f126d9c7877a397d203454
Julio Lopez <ljuliom@gmail.com>
2015-05-12 00:30:26 -0500

    more specs

--- test/test_reptar.rb
+++ test/test_reptar.rb
@@ -91,3 +91,152 @@ test "a single representable association" do
 
   assert_equal UserReptar.new(user).to_json, result
 end
+
+test "single representable association as nil" do
+  UserReptar = Class.new(Reptar) do
+    attribute :name
+    attribute :company, with: "CompanyReptar"
+  end
+  user = User.new(name: "Julio")
+  user.company = nil
+
+  result = {
+    name: "Julio",
+    company: nil
+  }.to_json
+
+  assert_equal UserReptar.new(user).to_json, result
+end
+
+test "array with association" do
+  UserReptar = Class.new(Reptar) do
+    attribute :name
+    attribute :company, with: "CompanyReptar"
+  end
+
+  CompanyReptar = Class.new(Reptar) do
+    attribute :name
+  end
+
+  users = [
+    User.new(name: "Julio", company: Company.new(name: "Codalot")),
+    User.new(name: "Piero", company: Company.new(name: "Velocis"))
+  ]
+
+  result = [
+    {
+      name: "Julio",
+      company: {
+        name: "Codalot"
+      }
+    },
+    {
+      name: "Piero",
+      company: {
+        name: "Velocis"
+      }
+    }
+  ].to_json
+
+  assert_equal UserReptar.new(users).to_json, result
+end
+
+test "serializes object with collection" do
+  UserReptar = Class.new(Reptar) do
+    attribute :name
+    collection :posts, with: "PostReptar"
+  end
+
+  PostReptar = Class.new(Reptar) do
+    attribute :title
+  end
+
+  user = User.new(name: "Julio")
+  user.posts = [
+    Post.new(title: "Awesome title"),
+    Post.new(title: "Cats are dominating the world right now")
+  ]
+
+  result = {
+    name: "Julio",
+    posts: [
+      {
+        title: "Awesome title"
+      },
+      {
+        title: "Cats are dominating the world right now"
+      }
+    ]
+  }.to_json
+
+  assert_equal UserReptar.new(user).to_json, result
+end
+
+test "array with representable collections" do
+  UserReptar = Class.new(Reptar) do
+    attribute :name
+    collection :posts, with: "PostReptar"
+  end
+
+  PostReptar = Class.new(Reptar) do
+    attribute :title
+  end
+
+  users = [
+    User.new(
+      name: "Julio",
+      posts: [
+        Post.new(title: "Hi, I'm a dog")
+      ]
+    ),
+    User.new(
+      name: "Piero",
+      posts: [
+        Post.new(title: "I like turtles"),
+        Post.new(title: "Please come back PT!"),
+      ]
+    )
+  ]
+
+  result = [
+    {
+      name: "Julio",
+      posts: [
+        {
+          title: "Hi, I'm a dog"
+        }
+      ]
+    },
+    {
+      name: "Piero",
+      posts: [
+        {
+          title: "I like turtles"
+        },
+        {
+          title: "Please come back PT!"
+        }
+      ]
+    }
+  ].to_json
+
+  assert_equal UserReptar.new(users).to_json, result
+end
+
+test "custom association method" do
+  PostReptar = Class.new(Reptar) do
+    attribute :title
+  end
+
+  UserReptar = Class.new(Reptar) do
+    collection :posts, with: "PostReptar"
+
+    def posts
+      [Post.new(title: "I like turtles")]
+    end
+  end
+
+  user = User.new
+  result = { posts: [{title: "I like turtles" }] }.to_json
+  assert_equal UserReptar.new(user).to_json, result
+end
