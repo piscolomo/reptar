ff9042a21e55c533b6f7e0e773127b46958a3043
Julio Lopez <ljuliom@gmail.com>
2015-05-11 21:08:42 -0500

    methods as attributes to json

--- lib/reptar.rb
+++ lib/reptar.rb
@@ -14,11 +14,15 @@ class Reptar
     end
   end
 
+  def method_missing(method_name)
+    @object.respond_to?(method_name) ? @object.send(method_name) : super
+  end
+
   def to_json
     return nil unless @object
     hash = {}
     self.class.attributes.each do |name|
-      hash[name] = @object.send(name)
+      hash[name] = @object.respond_to?(name) ? @object.send(name) : self.send(name)
     end
     hash.to_json
   end
--- test/test_reptar.rb
+++ test/test_reptar.rb
@@ -2,6 +2,7 @@ require File.expand_path("../lib/reptar", File.dirname(__FILE__))
 require "ostruct"
 
 User = OpenStruct
+Post = OpenStruct
 
 class UserReptar < Reptar
   attribute :name
@@ -11,4 +12,18 @@ test "attributes to json" do
   user = User.new(name: "Julio")
   result = {name: "Julio"}.to_json
   assert_equal UserReptar.new(user).to_json, result
+end
+
+class PostReptar < Reptar
+  attribute :slug
+
+  def slug
+    "#{name}-#{id}"
+  end
+end
+
+test "methods as attributes to json" do
+  post = Post.new(name: "a-demo-post", id: 1)
+  result = {slug: "a-demo-post-1"}.to_json
+  assert_equal PostReptar.new(post).to_json, result
 end
